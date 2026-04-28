af4c0503159b2913e3c369c3c378a3b80a84c98e
Sajan10p <sajanpathakleo.10@gmail.com>
2026-04-28 11:02:55 +1000

    Add submission model

---
+++ Models/Submission.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projectpath.Models
+{
+    public class Submission
+    {
+        public int Id { get; set; }
+
+        public int AssignmentId { get; set; }
+        public Assignment Assignment { get; set; }
+
+        public string StudentId { get; set; }
+        public ApplicationUser Student { get; set; }
+
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        public DateTime SubmittedAt { get; set; } = DateTime.Now;
+    }
+}
